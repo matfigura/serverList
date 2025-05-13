733ccc6a1864a17f4ad24ac8eb83628b18e6a210
matfigura <mateuszbryla94@gmail.com>
2025-05-13 21:31:21 +0200

    Add .env and config.py for Flask environment configuration

---
+++ flaskr/config/config.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+load_dotenv(os.path.join(base_dir, '.env'))
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'defaultkey')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_URL = os.getenv('REDIS_URL')
+    CORS_HEADERS = 'Content-Type'
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    ENV = 'development'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ENV = 'production'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
+config_dict = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
